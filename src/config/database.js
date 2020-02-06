7e331d3e1e44584ea2c33293a26691a604d133ef
michelspirlandeli <michelspirlandeli10@gmail.com>
2020-02-06 13:55:23 -0300

    add Configurando Sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'postgresdocker',
+  database: 'dbgobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    uderscoredAll: true,
+  },
+};
