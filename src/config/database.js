a228c9bc0715896632b06dccf54274eba904dab9
michelspirlandeli <michelspirlandeli10@gmail.com>
2020-02-25 16:16:09 -0300

    update

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
-  password: 'postgresdocker',
+  password: 'postgres',
   database: 'dbgobarber',
   define: {
     timestamps: true,
