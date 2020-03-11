0f79ed1fed36b239aadbab02ddcb5ad3eb84d865
michelspirlandeli <michelspirlandeli10@gmail.com>
2020-03-10 21:19:07 -0300

    Criando loader de models

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import routes from './router';
+import './database';
 
 class App {
   constructor() {
--- src/router.js
+++ src/router.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Michel spirlandeli',
+    email: 'michel@teste.com',
+    password_hash: '123456789'
+  });
+
+  return res.json(user);
 });
 
 export default routes;
